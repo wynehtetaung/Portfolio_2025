c02dce1cd8aac3534fde6d66351985b4ab746efa
wynehtetaung <wynehtet2022@gmail.com>
2025-08-21 22:40:19 +0630

    [done] app drawer

--- src/components/AppNav.jsx
+++ src/components/AppNav.jsx
@@ -3,6 +3,8 @@ import {
   Box,
   Drawer,
   IconButton,
+  List,
+  ListItem,
   Toolbar,
   Typography,
 } from "@mui/material";
@@ -11,8 +13,12 @@ import Logo from "../assets/images/Freddie....png";
 import { img } from "framer-motion/client";
 import { useState } from "react";
 import { Close as CloseIcon } from "@mui/icons-material";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 
 const AppNav = () => {
+  const { pathname } = useLocation();
+  const { sub, id } = useParams();
+  const navigate = useNavigate();
   const [open, setOpen] = useState(false);
   const handleDrawer = (state) => {
     setOpen(state);
@@ -67,16 +73,73 @@ const AppNav = () => {
             mt: "35px",
           }}
         >
-          Hello
-          <Box component={"button"} onClick={() => handleDrawer(false)}>
-            close
-          </Box>
           <Box sx={{ display: "flex", justifyContent: "space-between" }}>
             <Box component={"img"} src={Logo} width={111} height={29} />
-            <IconButton sx={{ mr: "25px" }}>
+            <IconButton sx={{ mr: "25px" }} onClick={() => handleDrawer(false)}>
               <CloseIcon sx={{ width: 34, height: 34, color: "#ff0b55" }} />
             </IconButton>
           </Box>
+          <List>
+            <ListItem>
+              <Typography
+                onClick={() => navigate("/")}
+                sx={{
+                  fontSize: 25,
+                  color: pathname === "/" ? "#ff0b55" : "#9e9e9e",
+                  ":hover": {
+                    color: "#ff0b55",
+                  },
+                }}
+              >
+                HOME
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography
+                onClick={() => navigate("/my-work/app")}
+                sx={{
+                  fontSize: 25,
+                  color:
+                    pathname === `/my-work/${sub}` ||
+                    pathname === `/my-work/${sub}/${id}`
+                      ? "#ff0b55"
+                      : "#9e9e9e",
+                  ":hover": {
+                    color: "#ff0b55",
+                  },
+                }}
+              >
+                MY WORKS
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography
+                onClick={() => navigate("/about")}
+                sx={{
+                  fontSize: 25,
+                  color: pathname === "/about" ? "#ff0b55" : "#9e9e9e",
+                  ":hover": {
+                    color: "#ff0b55",
+                  },
+                }}
+              >
+                ABOUT
+              </Typography>
+            </ListItem>
+            <ListItem>
+              <Typography
+                sx={{
+                  fontSize: 25,
+                  color: pathname === "/contact" ? "#ff0b55" : "#9e9e9e",
+                  ":hover": {
+                    color: "#ff0b55",
+                  },
+                }}
+              >
+                CONTACT
+              </Typography>
+            </ListItem>
+          </List>
         </Box>
       </Drawer>
     </Box>
